b7d787e2b551f136291aa002e787ba95eae9b25f
maksim789456 <maksim_789456@mail.ru>
2023-11-27 06:13:01 +0400

    Base setup
    - init necessary packages
    - remove all 'create next' code
    - add dg color, setup Jost font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+const colors = require('tailwindcss/colors')
 
 const config: Config = {
   content: [
@@ -7,12 +8,27 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      blue: colors.blue,
+      red: colors.red,
+      dg: '#231f20'
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        'dg-main': 'var(--font-jost)'
       },
+      
+      gridTemplateColumns: {
+        '15': 'repeat(15, minmax(0, 1fr))'
+      }
     },
   },
   plugins: [],
