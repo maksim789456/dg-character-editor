c606c5684ed7f2012036348f17b5c22f0f36b980
maksim789456 <maksim_789456@mail.ru>
2023-12-04 21:01:22 +0400

    Remove access to field in personal section, add in textInput direct bind to redux store by name

--- components/characterSheet/sections/PersonalSection.tsx
+++ components/characterSheet/sections/PersonalSection.tsx
@@ -4,8 +4,6 @@ import React from "react";
 import Category from "../category";
 import GenderInput from "../genderInput";
 import TextInput from "../textInput";
-import { useDispatch, useSelector } from "react-redux";
-import { set } from "@/src/features/dgCharacter/dgCharacterSlice";
 
 interface PersonalSectionProps extends React.HTMLAttributes<HTMLDivElement> {
   sectionLocale: any;
@@ -15,9 +13,6 @@ const PersonalSection: React.FC<PersonalSectionProps> = ({
   sectionLocale,
   ...props
 }) => {
-  const dgCharacter = useSelector((state: any) => state.dgCharacter);
-  const dispatch = useDispatch();
-
   return (
     <Category
       className="col-span-2"
@@ -28,34 +23,22 @@ const PersonalSection: React.FC<PersonalSectionProps> = ({
         <TextInput
           className="col-span-3"
           title={sectionLocale?.fullName}
-          disabled={!dgCharacter.editMode}
-          value={dgCharacter.fullName}
-          onValueChange={(value) => dispatch(set({ field: "fullName", value }))}
+          name="fullName"
         />
         <TextInput
           className="col-span-2"
           title={sectionLocale?.profession}
-          disabled={!dgCharacter.editMode}
-          value={dgCharacter.profession}
-          onValueChange={(value) =>
-            dispatch(set({ field: "profession", value }))
-          }
+          name="profession"
         />
         <TextInput
           className="col-span-3"
           title={sectionLocale?.employer}
-          disabled={!dgCharacter.editMode}
-          value={dgCharacter.employer}
-          onValueChange={(value) => dispatch(set({ field: "employer", value }))}
+          name="employer"
         />
         <TextInput
           className="col-span-2"
           title={sectionLocale?.nationality}
-          disabled={!dgCharacter.editMode}
-          value={dgCharacter.nationality}
-          onValueChange={(value) =>
-            dispatch(set({ field: "nationality", value }))
-          }
+          name="nationality"
         />
         <GenderInput
           className="col-span-1"
@@ -66,16 +49,12 @@ const PersonalSection: React.FC<PersonalSectionProps> = ({
         <TextInput
           className="col-span-1"
           title={sectionLocale?.age}
-          disabled={!dgCharacter.editMode}
-          value={dgCharacter.age}
-          onValueChange={(value) => dispatch(set({ field: "age", value }))}
+          name="age"
         />
         <TextInput
           className="col-span-3"
           title={sectionLocale?.education}
-          disabled={!dgCharacter.editMode}
-          value={dgCharacter.education}
-          onValueChange={(value) => dispatch(set({ field: "education", value }))}
+          name="education"
         />
       </div>
     </Category>
