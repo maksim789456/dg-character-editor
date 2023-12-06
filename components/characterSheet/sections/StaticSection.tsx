94c3cb810ad20594d5b59f11b21e334ae57961bc
maksim789456 <maksim_789456@mail.ru>
2023-12-06 04:41:59 +0400

    Extract calc stat in own component, remove direct access to state in static section

--- components/characterSheet/sections/StaticSection.tsx
+++ components/characterSheet/sections/StaticSection.tsx
@@ -2,21 +2,11 @@
 
 import React from "react";
 import Category from "../category";
-import TextInput from "../textInput";
-import TableInput from "../table/tableInput";
 import TableItem from "../table/tableItem";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  DgCharacter,
-  DgCharacterBaseStat,
-  DgCharacterStats,
-} from "@/src/model/character";
-import {
-  setBaseStat,
-  setBaseStatDescription,
-  setStat,
-} from "@/src/features/dgCharacter/dgCharacterSlice";
 import BaseStat from "../baseStat";
+import { RootState } from "@/src/store/store";
+import CalcStat from "../calcStat";
+import { maxHpSelector, maxWpSelector, maxSanSelector } from "@/src/redux/selectors";
 
 interface StaticSectionProps extends React.HTMLAttributes<HTMLDivElement> {
   sectionLocale: any;
@@ -26,11 +16,7 @@ const StaticSection: React.FC<StaticSectionProps> = ({
   sectionLocale,
   ...props
 }) => {
-  const dgCharacter = useSelector(
-    (state: any) => state.dgCharacter
-  ) as DgCharacter;
-  const dispatch = useDispatch();
-  const { editMode } = dgCharacter;
+  const maxBpSelector = (_: RootState) => sectionLocale?.bpStatMax;
   return (
     <Category name={sectionLocale?.categoryName} {...props}>
       <div className="grid grid-cols-1 grid-rows-5">
@@ -56,32 +42,32 @@ const StaticSection: React.FC<StaticSectionProps> = ({
           <BaseStat
             sectionLocale={sectionLocale}
             title={sectionLocale?.strStat}
-            name={"str"}
+            name="str"
           />
           <BaseStat
             sectionLocale={sectionLocale}
             title={sectionLocale?.conStat}
-            name={"con"}
+            name="con"
           />
           <BaseStat
             sectionLocale={sectionLocale}
             title={sectionLocale?.dexStat}
-            name={"dex"}
+            name="dex"
           />
           <BaseStat
             sectionLocale={sectionLocale}
             title={sectionLocale?.intStat}
-            name={"int"}
+            name="int"
           />
           <BaseStat
             sectionLocale={sectionLocale}
             title={sectionLocale?.powStat}
-            name={"pow"}
+            name="pow"
           />
           <BaseStat
             sectionLocale={sectionLocale}
             title={sectionLocale?.chaStat}
-            name={"cha"}
+            name="cha"
           />
         </div>
         <div className="grid grid-cols-1 grid-rows-5 row-span-2">
@@ -100,84 +86,28 @@ const StaticSection: React.FC<StaticSectionProps> = ({
               isHeader={true}
             />
           </div>
-          <div className="grid grid-cols-4">
-            <TableItem
-              className="col-span-2 py-1"
-              title={sectionLocale?.hpStat}
-            />
-            <TableItem
-              title={`${Math.round(
-                (dgCharacter.stats.str.score + dgCharacter.stats.con.score) / 2
-              )}`}
-              isHeader={true}
-              fontSize="text-base"
-            />
-            <TableInput
-              isNumber={true}
-              value={dgCharacter.stats.hp}
-              onValueChange={(value) => {
-                dispatch(setStat({ field: "hp", value }));
-              }}
-            />
-          </div>
-          <div className="grid grid-cols-4">
-            <TableItem
-              className="col-span-2 py-1"
-              title={sectionLocale?.wpStat}
-            />
-            <TableItem
-              title={`${dgCharacter.stats.pow.score}`}
-              isHeader={true}
-              fontSize="text-base"
-            />
-            <TableInput
-              isNumber={true}
-              value={dgCharacter.stats.wp}
-              onValueChange={(value) =>
-                dispatch(setStat({ field: "wp", value }))
-              }
-            />
-          </div>
-          <div className="grid grid-cols-4">
-            <TableItem
-              className="col-span-2 py-1"
-              title={sectionLocale?.sanStat}
-            />
-            <TableItem
-              title={`${
-                99 -
-                (dgCharacter.skills.find((p) => p.id === "unnatural")
-                  ?.characterSkillRate ?? 0)
-              }`}
-              isHeader={true}
-              fontSize="text-base"
-            />
-            <TableInput
-              isNumber={true}
-              value={dgCharacter.stats.san}
-              onValueChange={(value) =>
-                dispatch(setStat({ field: "san", value }))
-              }
-            />
-          </div>
-          <div className="grid grid-cols-4">
-            <TableItem
-              className="col-span-2 py-1"
-              title={sectionLocale?.bpStat}
-            />
-            <TableItem
-              title={sectionLocale?.bpStatMax}
-              isHeader={true}
-              fontSize="text-base"
-            />
-            <TableItem
-              isHeader={true}
-              fontSize="text-base"
-              title={`${dgCharacter.stats.bp}`}
-            />
-          </div>
+          <CalcStat
+            title={sectionLocale?.hpStat}
+            name="hp"
+            maxSelector={maxHpSelector}
+          />
+          <CalcStat
+            title={sectionLocale?.wpStat}
+            name="wp"
+            maxSelector={maxWpSelector}
+          />
+          <CalcStat
+            title={sectionLocale?.sanStat}
+            name="san"
+            maxSelector={maxSanSelector}
+          />
+          <CalcStat
+            title={sectionLocale?.bpStat}
+            name="bp"
+            maxSelector={maxBpSelector}
+          />
         </div>
-        <TextInput
+        {/* <TextInput
           title={sectionLocale?.description}
           multiline={true}
           disabled={!editMode}
@@ -185,7 +115,7 @@ const StaticSection: React.FC<StaticSectionProps> = ({
           onValueChange={(value) =>
             dispatch(setStat({ field: "description", value }))
           }
-        />
+        /> */}
       </div>
     </Category>
   );
