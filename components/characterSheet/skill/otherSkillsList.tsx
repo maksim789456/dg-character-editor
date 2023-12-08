1274ee28c0bfc24783b6cd2a2aa02a59b25f2868
maksim789456 <maksim_789456@mail.ru>
2023-12-08 22:46:28 +0400

    Fix un-memoized selector in other skills list

--- components/characterSheet/skill/otherSkillsList.tsx
+++ components/characterSheet/skill/otherSkillsList.tsx
@@ -1,13 +1,16 @@
 import { useAppSelector } from "@/src/redux/hooks";
 import OtherSkill from "./otherSkill";
+import { shallowEqual } from "react-redux";
 
 interface SkillsListProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const OtherSkillsList: React.FC<SkillsListProps> = ({}) => {
-  const otherSkillsIds = useAppSelector((state) =>
-    state.dgCharacter.skills
-      .filter((skill) => skill.isOther)
-      .map((skill) => skill.id)
+  const otherSkillsIds = useAppSelector(
+    (state) =>
+      state.dgCharacter.skills
+        .filter((skill) => skill.isOther)
+        .map((skill) => skill.id),
+    shallowEqual
   );
   return otherSkillsIds.map((skillId) => (
     <OtherSkill skillId={skillId} key={skillId} />
