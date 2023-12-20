da57dc7a86156ec259c40cd863cf669c07ee5603
maksim789456 <maksim_789456@mail.ru>
2023-12-20 10:51:19 +0400

    Change old lang redirect to nextjs variant

---
+++ middleware.ts
@@ -0,0 +1,36 @@
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+import { NextResponse, type NextRequest } from "next/server";
+
+const locales = ["en", "ru"];
+const defaultLocale = "en";
+
+function getLocale(request: NextRequest): string {
+  let acceptLang = request.headers.get("accept-language") ?? "en-US,en;q=0.5";
+  let languages = new Negotiator({
+    headers: { "accept-language": acceptLang },
+  }).languages();
+  return match(languages, locales, defaultLocale);
+}
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+  );
+
+  if (pathnameHasLocale) return;
+
+  const locale = getLocale(request);
+  request.nextUrl.pathname = `/${locale}`;
+  return NextResponse.redirect(request.nextUrl);
+}
+
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next)
+    "/((?!_next).*)",
+    // Optional: only run on root (/) URL
+    // '/'
+  ],
+};
