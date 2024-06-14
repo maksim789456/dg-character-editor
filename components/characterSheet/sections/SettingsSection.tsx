2ee31bd0aa3ff9a839d35c40b8d57ef7f5c434e9
maksim789456 <maksim_789456@mail.ru>
2024-06-14 04:15:58 +0400

    Add lang change button (#4)

--- components/characterSheet/sections/SettingsSection.tsx
+++ components/characterSheet/sections/SettingsSection.tsx
@@ -2,6 +2,7 @@ import ExportButton from "../export/exportButton";
 import { set } from "@/src/features/dgCharacter/dgCharacterSlice";
 import React from "react";
 import { useAppDispatch, useAppSelector } from "@/src/redux/hooks";
+import LangSelect from "@/components/langSelect";
 
 interface SettingsSectionProps extends React.HTMLAttributes<HTMLDivElement> {
   sectionLocale: any;
@@ -17,21 +18,23 @@ const SettingsSection: React.FC<SettingsSectionProps> = ({
   const dispatch = useAppDispatch();
 
   return (
-    <div
-      className={`flex flex-row justify-center items-center gap-3 ${props.className}`}
-    >
-      <div className="flex flex-row gap-1">
-        <p className="font-dg-main text-dg">{sectionLocale?.editMode}</p>
-        <input
-          type="checkbox"
-          checked={editMode}
-          onChange={(e) =>
-            dispatch(set({ field: "editMode", value: e.target.checked }))
-          }
-        />
+    <div className={`grid grid-cols-5 items-center gap-3 ${props.className}`}>
+      <div className="flex flex-row items-center justify-center gap-3 col-start-2 col-span-3">
+        <div className="flex flex-row gap-1">
+          <p className="font-dg-main text-dg">{sectionLocale?.editMode}</p>
+          <input
+            type="checkbox"
+            checked={editMode}
+            onChange={(e) =>
+              dispatch(set({ field: "editMode", value: e.target.checked }))
+            }
+          />
+        </div>
+        <ExportButton lang={lang}>{sectionLocale?.exportPdf}</ExportButton>
       </div>
-      <ExportButton lang={lang}>{sectionLocale?.exportPdf}</ExportButton>
-      {props.children}
+      <div className="ml-auto bg-dg/90 rounded p-1.5">
+        <LangSelect lang={lang} />
+      </div>
     </div>
   );
 };
---
+++ components/langSelect.tsx
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+import { GB, RU } from "country-flag-icons/react/1x1";
+import clsx from "clsx";
+
+interface LangSelectProps extends React.HTMLAttributes<HTMLDivElement> {
+  lang: string;
+}
+
+const LangSelect: React.FC<LangSelectProps> = ({ lang, ...props }) => {
+  const langs = [
+    { id: "en", icon: <GB className="w-7 h-5" /> },
+    { id: "ru", icon: <RU className="w-7 h-5" /> },
+  ];
+
+  return (
+    <div className={clsx("flex flex-row gap-3", props.className)}>
+      {langs.map((value, key) => {
+        if (value.id === lang) return;
+        return (
+          <a key={key} href={value.id}>
+            {value.icon}
+          </a>
+        );
+      })}
+    </div>
+  );
+};
+
+LangSelect.propTypes = {
+  className: PropTypes.string,
+  lang: PropTypes.string.isRequired,
+};
+
+export default LangSelect;
