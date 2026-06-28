09ec2f392d4afdf1509195974045345e1a45b161
maksim789456 <git@capybaraesttravy.ru>
2026-06-28 07:51:53 +0400

    Remove old lang code

--- app/[lang]/[id]/page.tsx
+++ app/[lang]/[id]/page.tsx
@@ -6,14 +6,12 @@ export default async function CharacterSheetPage({
 }: {
   params: { lang: string; id: string };
 }) {
-  const langDict = await getLocaleDictionary(params.lang);
   const skillsDict = await getSkillsDictionary(params.lang);
 
   return (
     <CharacterSheet
       lang={params.lang}
       id={params.id}
-      langDict={langDict}
       skillsDict={skillsDict}
     />
   );
