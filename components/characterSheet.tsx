8cf17c866c72894bfcf75b9f6c391669debe49c2
maksim789456 <git@capybaraesttravy.ru>
2026-07-21 03:41:17 +0400

    Adding root for toasts

--- components/characterSheet.tsx
+++ components/characterSheet.tsx
@@ -15,6 +15,7 @@ import DownloadJsonButton from "./characterSheet/export/downloadJsonButton";
 import ThemeProvider from "@/app/contexts/themeContext";
 import clsx from "clsx";
 import { useTranslations } from "next-intl";
+import { Toaster, toast, useSonner } from "sonner";
 
 export default function CharacterSheet({
   lang,
@@ -24,6 +25,13 @@ export default function CharacterSheet({
   skillsDict: any;
 }) {
   const t = useTranslations("characterSheet");
+  const { toasts } = useSonner();
+
+  const onRemoveToastsClick = (e: React.MouseEvent<any>) => {
+    function removeAllToasts() {
+      toasts.forEach((t) => toast.dismiss(t.id));
+    }
+  };
 
   return (
     <main
@@ -31,6 +39,7 @@ export default function CharacterSheet({
       role="main"
       className="min-h-screen px-0 py-1 bg-white dark:bg-neutral-900 min-w-max sm:flex sm:flex-col sm:items-center sm:justify-between"
     >
+      <Toaster position="bottom-left" expand={true} visibleToasts={5}/>
       <div className="w-[40rem] md:w-[60rem] grid grid-cols-2 gap-0.5 gap-x-1">
         <Provider store={store}>
           <ThemeProvider>
